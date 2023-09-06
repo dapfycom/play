8829563524d246e8b89dd8ed6bfd0ce3cd12f880
Armando Cesar Martin Calderón <armandoc9943business@gmail.com>
2023-09-06 09:04:56 -0400

    3. Put title The Dust Converter, with tag “Our Dust Converter offers the smallest fees you'll find anywhere, outdoing all competitors. 💸🔥

--- src/views/DustView/DustView.tsx
+++ src/views/DustView/DustView.tsx
@@ -1,12 +1,18 @@
-import { Box, Center } from "@chakra-ui/react";
+import { Box, Center, Tooltip } from "@chakra-ui/react";
 import { MetaHead } from "components/MetaHead/MetaHead";
+import MyHeading from "components/MyHeading/MyHeading";
 import MoonDustXCard from "./components/ConvertCard/MoonDustXCard";
 
 const DustView = () => {
   return (
     <>
       <MetaHead metaTitle="Dust" />
-      <Center>
+      <Center flexDir={"column"}>
+        <Tooltip label="Our Dust Converter offers the smallest fees you'll find anywhere, outdoing all competitors. 💸🔥">
+          <Box mb={10}>
+            <MyHeading> The Dust Converter</MyHeading>
+          </Box>
+        </Tooltip>
         <Box maxW={"750px"} w="80%">
           <MoonDustXCard />
         </Box>
